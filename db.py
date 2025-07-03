3cc391fcfe23b383915e63c99cc4fd13212ef56b
Ezegor <Cprogramer08@gmail.com>
2025-07-03 15:37:21 +0300

    adeed password to db

--- db.py
+++ db.py
@@ -1,9 +1,14 @@
 from pymongo import MongoClient
+import os
 
 class Db:
     def __init__(self, cses_client, collection_name):
         self.cses_client = cses_client
-        client = MongoClient('mongodb://mongo:27017')
+
+        username = os.getenv("MONGO_INITDB_ROOT_USERNAME")
+        password = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
+
+        client = MongoClient(f'mongodb://{username}:{password}@mongo:27017')
         db = client.cses
         self.users = db[collection_name]
 
